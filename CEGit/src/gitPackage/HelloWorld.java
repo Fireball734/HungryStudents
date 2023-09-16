fa4460a51c7eaf0849a0d8af21df4916ec8a5ff1
Fireball734 <fireballflareblitz734@gmail.com>
2023-09-16 14:35:58 -0400

    please work this time

--- CEGit/src/gitPackage/HelloWorld.java
+++ CEGit/src/gitPackage/HelloWorld.java
@@ -5,7 +5,14 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
+<<<<<<< master
 		System.out.println("Oh god, it hurts");
+=======
+		int a = 1;
+		
+		a++;
+		System.out.println(a);
+>>>>>>> 262e210 Code begs you to stay
 	}
 
 }
