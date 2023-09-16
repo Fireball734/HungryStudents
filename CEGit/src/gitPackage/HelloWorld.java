262e210ed7b0944e2cc997c738ffcc77906b21a7
Fireball <Fireball@BOOK-IIUBRKHI8Q.fios-router.home>
2023-09-16 14:04:56 -0400

    Code begs you to stay

--- CEGit/src/gitPackage/HelloWorld.java
+++ CEGit/src/gitPackage/HelloWorld.java
@@ -5,10 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
-		int a = 1;
-		
-		a++;
-		System.out.println(a);
+		System.out.println("Please don't go...");
 	}
 
 }
