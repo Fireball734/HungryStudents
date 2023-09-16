10ea0dab65805b1c4509b4ee806d8c566ddf03ba
Fireball <Fireball@BOOK-IIUBRKHI8Q.fios-router.home>
2023-09-16 14:04:11 -0400

    Code now prints out the number 2

--- CEGit/src/gitPackage/HelloWorld.java
+++ CEGit/src/gitPackage/HelloWorld.java
@@ -5,7 +5,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
-		System.out.println("Oh god, it hurts");
+		int a = 1;
+		
+		a++;
+		System.out.println(a);
 	}
 
 }
